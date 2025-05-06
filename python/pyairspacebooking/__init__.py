8bcf295def0af486bfbc7cfdf5ac2abded941337
Aliaksei Pilko <a.pilko@soton.ac.uk>
2025-05-07 00:30:50 +0100

    Update python build

--- python/pyairspacebooking/__init__.py
+++ python/pyairspacebooking/__init__.py
@@ -1,21 +1,34 @@
 from __future__ import annotations
-from ctypes import cdll
-import os
-from pathlib import Path
+# from ctypes import cdll
+# import os
+# from pathlib import Path
 
-current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
+# current_dir = Path(os.path.dirname(os.path.realpath(__file__)))
 
-if os.path.exists(current_dir / 'lib'):
-    lib_dir = current_dir / 'lib'
-elif os.path.exists(current_dir / 'lib64'):
-    lib_dir = current_dir / 'lib64'
+# if os.path.exists(current_dir / 'lib'):
+#     lib_dir = current_dir / 'lib'
+# elif os.path.exists(current_dir / 'lib64'):
+#     lib_dir = current_dir / 'lib64'
 
-for lib in lib_dir.glob('*'):
-    cdll.LoadLibrary(lib)
+# for lib in lib_dir.glob('*'):
+#     cdll.LoadLibrary(lib)
 
-from ._pyairspacebooking import __doc__, __version__, get_H3_cell_bookings, get_S2_cell_bookings, get_H3D_cell_bookings, \
-    get_S23D_cell_bookings, CellBooking, StateVector4D, TimeSlice, Volume4D, get_H3_volume_bookings, \
-    get_S2_volume_bookings, get_H3D_volume_bookings, get_S23D_volume_bookings
+from ._pyairspacebooking import (
+    __doc__,
+    __version__,
+    get_H3_cell_bookings,
+    get_S2_cell_bookings,
+    get_H3D_cell_bookings,
+    get_S23D_cell_bookings,
+    CellBooking,
+    StateVector4D,
+    TimeSlice,
+    Volume4D,
+    get_H3_volume_bookings,
+    get_S2_volume_bookings,
+    get_H3D_volume_bookings,
+    get_S23D_volume_bookings,
+)
 
 __all__ = [
     "__doc__",
@@ -31,7 +44,7 @@ __all__ = [
     "get_H3_volume_bookings",
     "get_S2_volume_bookings",
     "get_H3D_volume_bookings",
-    "get_S23D_volume_bookings"
+    "get_S23D_volume_bookings",
 ]
 
 __dir__ = __all__
