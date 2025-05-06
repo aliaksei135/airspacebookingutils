83f270fce1445bfda1888481bf0b1cc0b8f9ecf0
Aliaksei Pilko <a.pilko@soton.ac.uk>
2025-05-07 00:30:38 +0100

    Update cmake for conan v2

--- conanfile.py
+++ conanfile.py
@@ -1,57 +1,26 @@
-from conans import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake
-from conan.tools.layout import cmake_layout
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 
-class abConan(ConanFile):
+class AbConan(ConanFile):
     name = "airspacebookingutils"
-    version = '0.1'
+    version = "0.1"
     author = "Aliaksei Pilko <dev@apilko.me>"
-    settings = "os", "compiler", "arch", "build_type"
     license = "Proprietary"
-    generators = "cmake_find_package_multi"
-    build_policy = 'missing'
-
+    settings = "os", "compiler", "arch", "build_type"
+    # package_type = "library"
     exports_sources = "CMakeLists.txt", "src/*"
+    requires = (
+        "eigen/[>=3.3.9]",
+        "h3/4.1.0",
+        "proj/9.5.0",
+        "geos/3.13.0",
+        "spdlog/[>=1.10.0]",
+        "s2geometry/0.11.1",
+    )
+    build_requires = ("doxygen/[>=1.9.1]",)
+    build_policy = "missing"
+    generators = "CMakeDeps", "CMakeToolchain"
 
-    def build_requirements(self):
-        self.build_requires("doxygen/[>=1.9.1]")
-
-    def requirements(self):
-        self.requires("eigen/[>=3.3.9]")
-        # self.requires("libcurl/[>=7.80.0]")
-        self.requires("abseil/20230125.2")  # Required by S2
-        self.requires("openssl/1.1.1t")  # Required by S2
-        self.requires("h3/4.1.0")
-        self.requires("proj/[>=8.1.1]")
-        self.requires("geos/[>=3.10.2]")
-        self.requires("spdlog/[>=1.10.0]")
-
-    def imports(self):
-        self.copy('*.dll', dst='bin', src='bin')  # Copies all dll files from packages bin folder to my "bin" folder
-        self.copy('*.dylib*', dst='bin',
-                  src='lib')  # Copies all dylib files from packages lib folder to my "bin" folder
-        self.copy('*.so*', dst='bin', src='lib')  # Copies all so files from packages lib folder to my "bin" folder
-
-    def layout(self):
-        cmake_layout(self)
-
-    def generate(self):
-        self.deps_cpp_info["doxygen"].set_property("skip_deps_file", True)
-
-        tc = CMakeToolchain(self)
-        tc.variables["BUILD_DOC"] = "ON"
-        tc.variables["CMAKE_FIND_PACKAGE_PREFER_CONFIG"] = "ON"
-        tc.generate()
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
-
-    def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = ['airspacebookingutils']
+    # def layout(self):
+    #     cmake_layout(self)
